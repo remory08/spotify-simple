a05d7d0d2ee6c2d9f3d62539721ec9799c234690
Ryne Emory <ryne@bluetent.com>
2016-01-31 20:09:52 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+require('dotenv').load();
+
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+  clientId : process.env.SPOTIFY_ID,
+  clientSecret : process.env.SPOTIFY_SECRET,
+  redirectUri : 'http://localhost:3000/callback'
+});
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+router.get('/searchTrack', function(req,res, next) {
+  spotifyApi.searchTracks(req.query.trackQuery)
+  .then(function(data) {
+    var tracks = data.body.tracks.items;
+    console.log(tracks);
+    res.render('index', {tracks: tracks});
+  }, function(err) {
+    console.error(err);
+  });
+});
+
+router.get('/searchArtistTracks', function(req,res,next) {
+  spotifyApi.searchTracks('artist:'+req.query.artistTrackQuery)
+  .then(function(data) {
+    var aTracks = data.body.tracks.items;
+    console.log('Search tracks by '+ req.query.artistTrackQuery + ' in the artist name', data.body.tracks.items);
+    res.render('tracks', {aTracks: aTracks})
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+})
+
+module.exports = router;
